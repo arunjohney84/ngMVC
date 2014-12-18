bef102bf9adcb71d53f1d6177e2edb183a9849b6
arunjohney84 <arunjohney@outlook.com>
2014-12-19 09:18:07 +1300

    FirstCommit

---
+++ ngMVC5/AngularForms/AngularForms/app/EmployeeForm/efDirective.js
@@ -0,0 +1,8 @@
+﻿
+angularFormsApp.directive('employeeForm',
+    function () {
+        return {
+            restrict: 'E', // E = use as an element
+            templateUrl: 'app/EmployeeForm/efTemplate.html'
+        };
+    });
---
+++ ngMVC5/AngularForms/AngularForms/app/HomeController.js
@@ -0,0 +1,16 @@
+﻿angularFormsApp.controller('HomeController',
+    function ($scope, $location, $modal, DataService) {
+        
+        $scope.showCreateEmployeeForm = function () {
+            // $location.path('/newEmployeeForm');
+            $modal.open({
+                templateUrl: "/app/EmployeeForm/efTemplate.html",
+                controller: "efController"
+            });
+        };
+        
+        $scope.showUpateEmployeeForm = function (id) {
+            $location.path('/updateEmployeeForm/' + id);
+        };
+        
+    });
